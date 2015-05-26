230ecfb0e409bff2cf57eb3337a345a90d9437d8
ddejongh <dimi.dejonghe@gmail.com>
2015-05-26 16:46:02 +0200

    modal try 1

--- js/components/edition.js
+++ js/components/edition.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import ImageViewer from './ascribe_media/image_viewer';
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
+import OverlayMixin from 'react-bootstrap/lib/OverlayMixin';
+import Modal from 'react-bootstrap/lib/Modal';
+import ModalTrigger from 'react-bootstrap/lib/ModalTrigger';
+import Modal from 'react-modal';
 
 /**
  * This is the component that implements display-specific functionality
  */
 let Edition = React.createClass({
-    //propTypes: {
-    //    title: React.PropTypes.string.isRequired
-    //},
-
     render() {
+        var modal = <ShareModal edition={this.props.edition} />;
         return (
             <div>
+                {modal}
                 <div className="col-md-7">
                     <ImageViewer thumbnail={this.props.edition.thumbnail}/>
                 </div>
                 <div className="col-md-5">
                     <EditionHeader edition={this.props.edition}/>
                     <EditionDetails edition={this.props.edition}/>
+
                 </div>
+
             </div>
         );
     }
@@ -69,5 +74,66 @@ let EditionDetailProperty = React.createClass({
     }
 });
 
+let ShareModal = React.createClass({
+    mixins: [OverlayMixin],
+
+    getInitialState: function() {
+        return {
+            isModalOpen: true
+        };
+    },
+    renderOverlay: function() {
+        if (!this.state.isModalOpen) {
+            return <span/>;
+        }
+    },
+    render: function() {
+        return (
+            <Modal title="Share artwork">
+            </Modal>
+        );
+    }
+});
+//let ShareModal = React.createClass({
+//    mixins: [OverlayMixin],
+//
+//    getInitialState: function() {
+//        return {
+//            isModalOpen: true
+//        };
+//    },
+//    renderOverlay: function() {
+//        if (!this.state.isModalOpen) {
+//            return <span/>;
+//        }
+//    },
+//    hide: function(){
+//        this.setState({isModalOpen: false});
+//    },
+//    render: function() {
+//        var content = "Hi,\n\nI am sharing \"" + this.props.edition.title +
+//                    "\" with you.\n\nTruly yours,\n";
+//        return (
+//            <Modal title="Share artwork" onRequestHide={this.hide}>
+//                <div className="modal-body">
+//                    <form id="share_modal_content" role="form">
+//                        <div className="form-group">
+//                            <input className="form-control input-text-ascribe" name="share_emails"
+//                                placeholder="Comma separated emails" required="required" type="text" />
+//                        </div>
+//                        <div className="form-group">
+//                            <textarea className="form-control input-text-ascribe" name="share_message"
+//                                defaultValue={content}></textarea>
+//                        </div>
+//                        <div className="modal-footer">
+//                            <button type="submit" className="btn btn-ascribe-inv">SHARE</button>
+//                            <button className="btn btn-ascribe" onClick={this.hide}>CLOSE</button>
+//                        </div>
+//                    </form>
+//                </div>
+//            </Modal>
+//        );
+//    }
+//});
 
 export default Edition;
