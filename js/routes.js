8ceabda1c5d9058a2a59d5687c14f1a3403c759c
Tim Daubenschütz <tim@ascribe.io>
2015-06-15 15:46:33 +0200

    add queryparams modal

--- js/routes.js
+++ js/routes.js
@@ -8,6 +8,7 @@ import PieceList from './components/piece_list';
 import EditionContainer from './components/edition_container';
 import PasswordResetForm from './components/ascribe_forms/form_password_reset';
 import AppConstants from './constants/application_constants';
+import LoginModalHandler from './components/login_modal_handler';
 
 let Route = Router.Route;
 let Redirect = Router.Redirect;
@@ -16,6 +17,7 @@ let baseUrl = AppConstants.baseUrl;
 
 let routes = (
     <Route name="app" path={baseUrl} handler={AscribeApp}>
+        <Route name="login" path="*" handler={LoginModalHandler} />
         <Route name="pieces" path="collection" handler={PieceList} />
         <Route name="edition" path="editions/:editionId" handler={EditionContainer} />
         <Route name="password_reset" path="password_reset" handler={PasswordResetForm} />
