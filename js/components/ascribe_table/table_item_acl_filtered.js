f6be4a29228c969a756e814f35f56805efbcc742
diminator <dimi.dejonghe@gmail.com>
2015-07-14 14:16:51 +0200

    consign WIP

--- js/components/ascribe_table/table_item_acl_filtered.js
+++ js/components/ascribe_table/table_item_acl_filtered.js
@@ -5,10 +5,18 @@ import React from 'react';
 
 let TableItemAclFiltered = React.createClass({
     propTypes: {
-        content: React.PropTypes.object.isRequired
+        content: React.PropTypes.object,
+        requestAction: React.PropTypes.string
     },
 
     render() {
+        if (this.props.requestAction){
+            return (
+                <span>
+                    {this.props.requestAction + ' request pending'}
+                </span>
+            );
+        }
         var availableAcls = ['acl_consign', 'acl_loan', 'acl_transfer', 'acl_view', 'acl_share', 'acl_unshare'];
 
         let filteredAcls = Object.keys(this.props.content).filter((key) => {
