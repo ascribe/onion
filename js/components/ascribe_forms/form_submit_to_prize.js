07f4cdd2e15ab1be3652d46652af1db27f94923c
Tim Daubenschütz <tim@ascribe.io>
2015-07-15 00:52:16 +0200

    advance prize submission form

--- js/components/ascribe_forms/form_submit_to_prize.js
+++ js/components/ascribe_forms/form_submit_to_prize.js
@@ -2,7 +2,12 @@
 
 import React from 'react';
 
-import Form from '../ascribe_forms/form';
+import Form from './form';
+import Property from './property';
+import InputTextAreaToggable from './input_textarea_toggable';
+import InputCheckbox from './input_checkbox';
+
+import Alert from 'react-bootstrap/lib/Alert';
 
 import AppConstants from '../../constants/application_constants';
 import ApiUrls from '../../constants/api_urls';
@@ -19,7 +24,9 @@ let PieceSubmitToPrizeForm = React.createClass({
     },
 
     getFormData() {
-        return this.props.piece;
+        return {
+            id: this.props.piece.id
+        };
     },
 
     render() {
@@ -41,8 +48,44 @@ let PieceSubmitToPrizeForm = React.createClass({
                     <div className="modal-footer">
                         <img src={AppConstants.baseUrl + 'static/img/ascribe_animated_small.gif'} />
                     </div>}>
-                <p>{getLangText('Are you sure you want to submit to the prize?')}</p>
-                <p>{getLangText('This is an irrevocable action%s', '.')}</p>
+                <Property
+                    name='artist_statement'
+                    label={getLangText('Artist statement')}
+                    editable={true}>
+                    <InputTextAreaToggable
+                        rows={1}
+                        editable={true}
+                        placeholder={getLangText('Enter your statement')}
+                        required="required"/>
+                </Property>
+                <Property
+                    name='work_description'
+                    label={getLangText('Work description')}
+                    editable={true}>
+                    <InputTextAreaToggable
+                        rows={1}
+                        editable={true}
+                        placeholder={getLangText('Enter the description for your work')}
+                        required="required"/>
+                </Property>
+                <Property
+                    name="terms"
+                    className="ascribe-settings-property-collapsible-toggle"
+                    style={{paddingBottom: 0}}>
+                    <InputCheckbox>
+                        <span>
+                            {' ' + getLangText('I agree to the Terms of Service the art price') + ' '}
+                            (<a href="https://s3-us-west-2.amazonaws.com/ascribe0/whitelabel/sluice/terms.pdf" target="_blank" style={{fontSize: '0.9em', color: 'rgba(0,0,0,0.7)'}}>
+                                {getLangText('read')}
+                            </a>)
+                        </span>
+                    </InputCheckbox>
+                </Property>
+                <Alert bsStyle='warning'>
+                    <p>{getLangText('Are you sure you want to submit to the prize?')}</p>
+                    <p>{getLangText('This is an irrevocable action%s', '.')}</p>
+                </Alert>
+                
             </Form>
         );
     }
