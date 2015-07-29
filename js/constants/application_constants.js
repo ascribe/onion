f0d6eaa950b07c1c6f410af44649c1be1137887a
Tim Daubenschütz <tim@ascribe.io>
2015-07-29 15:57:48 +0200

    update gitignore

--- js/constants/application_constants.js
+++ js/constants/application_constants.js
@@ -46,6 +46,13 @@ let constants = {
             'logo': 'https://s3-us-west-2.amazonaws.com/ascribe0/whitelabel/sluice/logo.jpeg',
             'permissions': ['register', 'edit', 'share', 'del_from_collection'],
             'type': 'prize'
+        },
+        {
+            'subdomain': 'cyland',
+            'name': 'Cyland',
+            'logo': 'https://s3-us-west-2.amazonaws.com/ascribe0/whitelabel/sluice/logo.jpeg',
+            'permissions': ['register', 'edit', 'share', 'del_from_collection'],
+            'type': 'prize'
         }
     ],
     'defaultDomain': {
