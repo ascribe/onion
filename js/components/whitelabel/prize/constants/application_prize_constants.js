84703779a92e397a227503b471b79794e4a314c1
Tim Daubenschütz <tim@ascribe.io>
2015-07-31 13:44:57 +0200

    rename prize endpoint to prizes

---
+++ js/components/whitelabel/prize/constants/application_prize_constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import AppConstants from '../../../../constants/application_constants';
+
+let constants = {
+    prizeApiEndpoint: AppConstants.apiEndpoint + 'prizes/'
+};
+
+export default constants;
