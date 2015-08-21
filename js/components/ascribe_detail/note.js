8b6c5e6e0f9467ddbfd5f08ebf803ba5c5db3cbb
diminator <dimi.dejonghe@gmail.com>
2015-08-21 16:39:06 +0200

    username boolean

--- js/components/ascribe_detail/note.js
+++ js/components/ascribe_detail/note.js
@@ -39,7 +39,7 @@ let Note = React.createClass({
     },
 
     render() {
-        if ((this.props.currentUser.username && true || false) && this.props.show) {
+        if (!!this.props.currentUser.username && this.props.show) {
             return (
                 <Form
                     url={this.props.url}
