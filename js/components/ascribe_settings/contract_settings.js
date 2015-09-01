b4abe7a24569f1a30f16d99f5d64fe31c31d23a9
Tim Daubenschütz <tim@ascribe.io>
2015-09-01 14:42:09 +0200

    generic form submission determination for fineuploader

--- js/components/ascribe_settings/contract_settings.js
+++ js/components/ascribe_settings/contract_settings.js
@@ -2,17 +2,9 @@
 
 import React from 'react';
 
-import Form from '../ascribe_forms/form';
-import Property from '../ascribe_forms/property';
-
 import CollapsibleParagraph from '../ascribe_collapsible/collapsible_paragraph';
+import CreateContractForm from '../ascribe_forms/form_create_contract';
 
-import ReactS3FineUploader from '../ascribe_uploader/react_s3_fine_uploader';
-
-import AppConstants from '../../constants/application_constants';
-import ApiUrls from '../../constants/api_urls';
-
-import { getCookie } from '../../utils/fetch_api_utils';
 import { getLangText } from '../../utils/lang_utils';
 
 
@@ -26,56 +18,9 @@ let ContractSettings = React.createClass({
             <CollapsibleParagraph
                 title={getLangText('Contract Settings')}
                 show={true}
-                defaultExpanded={this.props.defaultExpanded}>
-                <CollapsibleParagraph
-                    title={getLangText('Create new Contract')}
-                    show={true}
-                    defaultExpanded={this.props.defaultExpanded}>
-                    <Form>
-                        <Property
-                            label="Contract file">
-                            <ReactS3FineUploader
-                                keyRoutine={{
-                                    url: AppConstants.serverUrl + 's3/key/',
-                                    fileClass: 'contract'
-                                }}
-                                createBlobRoutine={{
-                                    url: ApiUrls.blob_contracts
-                                }}
-                                validation={{
-                                    itemLimit: 100000,
-                                    sizeLimit: '50000000'
-                                }}
-                                session={{
-                                    endpoint: ApiUrls.blob_contracts,
-                                    customHeaders: {
-                                        'X-CSRFToken': getCookie(AppConstants.csrftoken)
-                                    },
-                                    cors: {
-                                        expected: true,
-                                        sendCredentials: true
-                                    }
-                                }}
-                                signature={{
-                                    endpoint: AppConstants.serverUrl + 's3/signature/',
-                                    customHeaders: {
-                                       'X-CSRFToken': getCookie(AppConstants.csrftoken)
-                                    }
-                                }}
-                                deleteFile={{
-                                    enabled: true,
-                                    method: 'DELETE',
-                                    endpoint: AppConstants.serverUrl + 's3/delete',
-                                    customHeaders: {
-                                       'X-CSRFToken': getCookie(AppConstants.csrftoken)
-                                    }
-                                }}
-                                areAssetsDownloadable={true}
-                                areAssetsEditable={true}/>
-                        </Property>
-                        <hr />
-                    </Form>
-                </CollapsibleParagraph>
+                defaultExpanded={true}>
+                {/* this should be this.props.defaultExpanded */}
+                <CreateContractForm />
             </CollapsibleParagraph>
         );
     }
