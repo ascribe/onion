436ffabffbefcdbff616209ba41528ee20c2a702
vrde <agranzot@gmail.com>
2015-09-07 11:40:05 +0200

    Add custom buttons

--- js/lib/buttons.js
+++ js/lib/buttons.js
@@ -1,27 +1,53 @@
+/**
+ *
+ */
+
+'use strict';
+
 import React from 'react';
 import _Button from 'react-bootstrap/lib/Button';
-
-export const Button = React.createClass({
-
-    render: function render() {
-        return (
-            <_Button>
-                {this.props.children}
-            </_Button>
-        )
-    }
-
-});
+import classnames from 'classnames';
 
 
-export const SecondaryButton = React.createClass({
+const DISABLED_STATUSES = ['loading', 'disabled'];
 
-    render: function render() {
-        return (
-            <_Button className='btn-secondary'>
-                {this.props.children}
-            </_Button>
-        )
-    }
 
-});
+function ButtonFactory(btnClassName) {
+
+    let GenericButton = React.createClass({
+        propTypes: {
+            status: React.PropTypes.oneOf(['loading', 'disabled']),
+            children: React.PropTypes.oneOfType([React.PropTypes.arrayOf(React.PropTypes.element),
+                                                 React.PropTypes.element])
+        },
+
+        getInitialState: function() {
+            return {
+                status: this.props.status
+            };
+        },
+
+        render: function render() {
+            let disabled = DISABLED_STATUSES.indexOf(this.state.status) !== -1;
+            let className = '';
+
+            if (this.state.status !== 'disabled') {
+                className = this.state.status;
+            }
+
+            return (
+                <_Button bsStyle={btnClassName} className={classnames(className)} disabled={disabled}>
+                    {this.props.children}
+                </_Button>
+            );
+        }
+
+    });
+
+    return GenericButton;
+}
+
+
+export const Button = ButtonFactory('btn-primary');
+export const SecondaryButton = ButtonFactory('btn-secondary');
+export const DangerButton = ButtonFactory('btn-danger');
