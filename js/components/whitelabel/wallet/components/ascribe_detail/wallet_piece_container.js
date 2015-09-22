b4c7b864fd739bf4449e17249472df6912901bf5
diminator <dimi.dejonghe@gmail.com>
2015-09-22 09:20:10 +0200

    check if piece exists in props in wallet details

--- js/components/whitelabel/wallet/components/ascribe_detail/wallet_piece_container.js
+++ js/components/whitelabel/wallet/components/ascribe_detail/wallet_piece_container.js
@@ -17,7 +17,7 @@ import ApiUrls from '../../../../../constants/api_urls';
 
 import { getLangText } from '../../../../../utils/lang_utils';
 import { mergeOptions } from '../../../../../utils/general_utils';
-
+import AppConstants from '../../../../../constants/application_constants';
 
 let WalletPieceContainer = React.createClass({
 
@@ -30,54 +30,63 @@ let WalletPieceContainer = React.createClass({
 
     
     render() {
-        return (
-            <Piece
-                piece={this.props.piece}
-                loadPiece={this.loadPiece}
-                header={
-                    <div className="ascribe-detail-header">
-                        <hr style={{marginTop: 0}}/>
-                        <h1 className="ascribe-detail-title">{this.props.piece.title}</h1>
-                        <DetailProperty label="BY" value={this.props.piece.artist_name} />
-                        <DetailProperty label="DATE" value={ this.props.piece.date_created.slice(0, 4) } />
-                        <hr/>
-                    </div>
+        if(this.props.piece && this.props.piece.title) {
+            return (
+                <Piece
+                    piece={this.props.piece}
+                    loadPiece={this.loadPiece}
+                    header={
+                        <div className="ascribe-detail-header">
+                            <hr style={{marginTop: 0}}/>
+                            <h1 className="ascribe-detail-title">{this.props.piece.title}</h1>
+                            <DetailProperty label="BY" value={this.props.piece.artist_name} />
+                            <DetailProperty label="DATE" value={ this.props.piece.date_created.slice(0, 4) } />
+                            <hr/>
+                        </div>
                     }
-                subheader={
+                    subheader={
                     <div className="ascribe-detail-header">
                         <DetailProperty label={getLangText('REGISTREE')} value={ this.props.piece.user_registered } />
                         <DetailProperty label={getLangText('ID')} value={ this.props.piece.bitcoin_id } ellipsis={true} />
                         <hr/>
                     </div>
                 }>
-                <WalletActionPanel
-                    piece={this.props.piece}
-                    currentUser={this.props.currentUser}
-                    loadPiece={this.loadPiece}
-                    submitButtonType={this.props.submitButtonType} />
-                <CollapsibleParagraph
-                    title={getLangText('Loan History')}
-                    show={this.props.piece.loan_history && this.props.piece.loan_history.length > 0}>
-                    <HistoryIterator
-                        history={this.props.piece.loan_history} />
-                </CollapsibleParagraph>
-                <CollapsibleParagraph
-                    title={getLangText('Notes')}
-                    show={!!(this.props.currentUser.username || this.props.piece.public_note)}>
-                    <Note
-                        id={() => {return {'id': this.props.piece.id}; }}
-                        label={getLangText('Personal note (private)')}
-                        defaultValue={this.props.piece.private_note || null}
-                        placeholder={getLangText('Enter your comments ...')}
-                        editable={true}
-                        successMessage={getLangText('Private note saved')}
-                        url={ApiUrls.note_private_piece}
-                        currentUser={this.props.currentUser}/>
-                </CollapsibleParagraph>
+                    <WalletActionPanel
+                        piece={this.props.piece}
+                        currentUser={this.props.currentUser}
+                        loadPiece={this.loadPiece}
+                        submitButtonType={this.props.submitButtonType}/>
+                    <CollapsibleParagraph
+                        title={getLangText('Loan History')}
+                        show={this.props.piece.loan_history && this.props.piece.loan_history.length > 0}>
+                        <HistoryIterator
+                            history={this.props.piece.loan_history}/>
+                    </CollapsibleParagraph>
+                    <CollapsibleParagraph
+                        title={getLangText('Notes')}
+                        show={!!(this.props.currentUser.username || this.props.piece.public_note)}>
+                        <Note
+                            id={() => {return {'id': this.props.piece.id}; }}
+                            label={getLangText('Personal note (private)')}
+                            defaultValue={this.props.piece.private_note || null}
+                            placeholder={getLangText('Enter your comments ...')}
+                            editable={true}
+                            successMessage={getLangText('Private note saved')}
+                            url={ApiUrls.note_private_piece}
+                            currentUser={this.props.currentUser}/>
+                    </CollapsibleParagraph>
 
-                {this.props.children}
-            </Piece>
-        );
+                    {this.props.children}
+                </Piece>
+            );
+        }
+        else {
+            return (
+                <div className="fullpage-spinner">
+                    <img src={AppConstants.baseUrl + 'static/img/ascribe_animated_medium.gif'} />
+                </div>
+            );
+        }
 
     }
 });
