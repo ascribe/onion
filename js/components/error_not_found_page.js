90cf137c2c6adc1f99e4d0320a37e7a8a76464c1
Tim Daubenschütz <tim@ascribe.io>
2015-09-29 16:00:58 +0200

    Integrate error page in app + whitelabel pages

---
+++ js/components/error_not_found_page.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+
+let ErrorNotFoundPage = React.createClass({
+    render() {
+        return (
+            <span>Hello</span>
+        );
+    }
+});
+
+export default ErrorNotFoundPage;
