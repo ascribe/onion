31a83a1d618fa52a61a674e3cec597cf2f9007a9
Tim Daubenschütz <tim@ascribe.io>
2015-10-02 11:03:42 +0200

    Add TransactionManager boilerplate code

--- js/components/ascribe_app.js
+++ js/components/ascribe_app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 import React from 'react';
+
+import TransactionManager from './ascribe_transaction_manager/transaction_manager';
+
 import Header from '../components/header';
 import Footer from '../components/footer';
 import GlobalNotification from './global_notification';
@@ -19,14 +22,17 @@ let AscribeApp = React.createClass({
         let { children, routes } = this.props;
 
         return (
-            <div className="container ascribe-default-app">
-                <Header routes={routes} />
-                {/* Routes are injected here */}
-                {children}
-                <Footer />
-                <GlobalNotification />
-                <div id="modal" className="container"></div>
-            </div>
+            <TransactionManager
+                routes={routes}>
+                <div className="container ascribe-default-app">
+                    <Header routes={routes} />
+                    {/* Routes are injected here */}
+                    {children}
+                    <Footer />
+                    <GlobalNotification />
+                    <div id="modal" className="container"></div>
+                </div>
+            </TransactionManager>
         );
     }
 });
