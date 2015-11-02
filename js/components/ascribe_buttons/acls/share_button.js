05e3069240cf1bede432f066c9b824a4f589cff7
Brett Sun <brett@ascribe.io>
2015-11-02 18:44:09 +0100

    Add propTypes to acl buttons

--- js/components/ascribe_buttons/acls/share_button.js
+++ js/components/ascribe_buttons/acls/share_button.js
@@ -4,9 +4,12 @@ import React from 'react';
 
 import AclButton from './acl_button';
 
+import { omitFromObject } from '../../../utils/general_utils';
 import { getLangText } from '../../../utils/lang_utils';
 
 let ShareButton = React.createClass({
+    propTypes: omitFromObject(AclButton.propTypes, ['action']),
+
     render() {
         return (
             <AclButton
