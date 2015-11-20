bccfc0906fb4534ccbea3fc8d0358cc53f6acf08
Tim Daubenschütz <tim@ascribe.io>
2015-11-20 16:17:08 +0100

    Revert super funny-"Important minor fix"
    We better want this not to go live
    
    This reverts commit b77fefb55c1a84b026d5365cd8150d4fb175f7e4.
    
    Conflicts:
    	js/constants/application_constants.js

--- js/constants/application_constants.js
+++ js/constants/application_constants.js
@@ -114,9 +114,6 @@ const constants = {
     },
     'twitter': {
         'sdkUrl': 'https://platform.twitter.com/widgets.js'
-    },
-    'fartscroll': {
-        'sdkUrl': 'https://rawgit.com/theonion/fartscroll.js/master/fartscroll.js'
     }
 };
 
