6649b7d7f187f0733ea6be3c655f611a875662bb
Brett Sun <brett@ascribe.io>
2015-11-21 19:23:20 +0100

    Implement pause and resume functionality to global notifications
    Also part of this change:
    * Handle the notification cooldown periods using status changes rather
    than emptying the queue
    * Refactor how notifications are rendered

--- js/stores/global_notification_store.js
+++ js/stores/global_notification_store.js
@@ -4,36 +4,63 @@ import { alt } from '../alt';
 
 import GlobalNotificationActions from '../actions/global_notification_actions';
 
+const GLOBAL_NOTIFICATION_COOLDOWN = 400;
+
 class GlobalNotificationStore {
     constructor() {
-        this.notificationQue = [];
+        this.notificationQueue = [];
+        this.notificationStatus = 'ready';
+        this.notificationsPaused = false;
 
         this.bindActions(GlobalNotificationActions);
     }
 
     onAppendGlobalNotification(newNotification) {
-        let notificationDelay = 0;
-        for(let i = 0; i < this.notificationQue.length; i++) {
-            notificationDelay += this.notificationQue[i].dismissAfter;
-        }
+        this.notificationQueue.push(newNotification);
 
-        this.notificationQue.push(newNotification);
-        setTimeout(GlobalNotificationActions.emulateEmptyStore, notificationDelay + newNotification.dismissAfter);
+        if (!this.notificationsPaused && this.notificationStatus === 'ready') {
+            this.showNextNotification();
+        }
     }
 
-    onEmulateEmptyStore() {
-        let actualNotificitionQue = this.notificationQue.slice();
+    showNextNotification() {
+        this.notificationStatus = 'show';
 
-        this.notificationQue = [];
+        setTimeout(GlobalNotificationActions.cooldownGlobalNotifications, this.notificationQueue[0].dismissAfter);
+    }
 
-        setTimeout(() => {
-            this.notificationQue = actualNotificitionQue.slice();
-            GlobalNotificationActions.shiftGlobalNotification();
-        }, 400);
+    onCooldownGlobalNotifications() {
+        // When still paused on cooldown, don't shift the queue so we can repeat the current notification.
+        if (!this.notificationsPaused) {
+            this.notificationStatus = 'cooldown';
+
+            // Leave some time between consecutive notifications
+            setTimeout(GlobalNotificationActions.shiftGlobalNotification, GLOBAL_NOTIFICATION_COOLDOWN);
+        } else {
+            this.notificationStatus = 'ready';
+        }
     }
 
     onShiftGlobalNotification() {
-        this.notificationQue.shift();
+        this.notificationQueue.shift();
+
+        if (!this.notificationsPaused && this.notificationQueue.length > 0) {
+            this.showNextNotification();
+        } else {
+            this.notificationStatus = 'ready';
+        }
+    }
+
+    onPauseGlobalNotifications() {
+        this.notificationsPaused = true;
+    }
+
+    onResumeGlobalNotifications() {
+        this.notificationsPaused = false;
+
+        if (this.notificationStatus === 'ready' && this.notificationQueue.length > 0) {
+            this.showNextNotification();
+        }
     }
 }
 
