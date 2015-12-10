d5823f10ecdface42caffe99e10955a100b2ad7b
Brett Sun <brett@ascribe.io>
2015-12-10 17:54:33 +0100

    Use mime types for input's accept property as safari doesn't play nicely with just the extensions

---
+++ js/constants/mime_types.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const MimeTypes = {
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'gif': 'image/gif',
+    'pdf': 'application/pdf',
+    'png': 'image/png'
+};
+
+export default MimeTypes;
