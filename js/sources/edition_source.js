1b4c6938f603ada02a16f91c223054298f497e88
Brett Sun <brett@ascribe.io>
2015-12-14 21:27:58 +0100

    Hotfix for iOS Safari mergeOptions clashes

--- js/sources/edition_source.js
+++ js/sources/edition_source.js
@@ -13,6 +13,24 @@ const EditionSource = {
 
         success: EditionActions.successFetchEdition,
         error: EditionActions.errorEdition
+    },
+
+    // For COA
+    lookupCoa: {
+        remote(state) {
+            return requests.get('coa', { id: state.edition.coa });
+        },
+
+        success: EditionActions.successFetchCoa,
+        error: EditionActions.errorCoa
+    },
+
+    performCreateCoa: {
+        remote(state) {
+            return requests.post('coa_create', {body: { bitcoin_id: state.edition.bitcoin_id }});
+        },
+        success: EditionActions.successFetchCoa,
+        error: EditionActions.errorCoa
     }
 };
 
--- js/stores/edition_store.js
+++ js/stores/edition_store.js
@@ -5,9 +5,6 @@ import { alt } from '../alt';
 import EditionActions from '../actions/edition_actions';
 
 import EditionSource from '../sources/edition_source';
-import CoaSource from '../sources/coa_source';
-
-import { mergeOptions } from '../utils/general_utils';
 
 
 class EditionStore {
@@ -22,7 +19,7 @@ class EditionStore {
         };
 
         this.bindActions(EditionActions);
-        this.registerAsync(mergeOptions(EditionSource, CoaSource));
+        this.registerAsync(EditionSource);
     }
 
     onFetchEdition(idToFetch) {
